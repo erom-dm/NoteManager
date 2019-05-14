b34fa02d98a4e770cfe1b62ec0f5ff004840bcc7
erom-dm <swiftyerom@gmail.com>
2019-05-14 23:50:41 +0300

    Initial project setup

---
+++ src/store/notes/index.js
@@ -0,0 +1,16 @@
+const TYPE = 'TYPE';
+
+const initialState = {
+  notes: []
+};
+
+const eventsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case TYPE:
+      return {...state, events: action.payload};
+    default:
+      return state
+  }
+};
+
+export default eventsReducer
