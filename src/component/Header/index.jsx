70d9de38c92e17281333dc316ffd2b0fd5e8d22b
erom-dm <swiftyerom@gmail.com>
2019-05-16 01:51:48 +0300

    Header styling

--- src/component/Header/index.jsx
+++ src/component/Header/index.jsx
@@ -3,7 +3,11 @@ import './header.scss';
 
 const Header = () => (
   <div className="header">
-      header here
+    <div className="header__display-modes" />
+    <div className="header__title">Note Manager</div>
+    <div className="header__btn-container">
+      <button className="header__add-new-btn" type="button">Add New</button>
+    </div>
   </div>
 );
 
